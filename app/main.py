fcd3f60971b050f4fe8e92d0408d229f2e9f1fb0
PKKnezevic <95423080+PKKnezevic@users.noreply.github.com>
2023-09-04 00:02:04 +0200

    Working function

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 
@@ -14,10 +14,10 @@ class Person(BaseModel):
     
     
 with open('people.json', 'r') as f:
-    people = json.load(f)['people']
+    people = json.load(f)
 
 @app.get('/person/{p_id}')
-def get_person(p_id: int):
+def get_person(p_id: int):  
     person = [p for p in people if p['id'] == p_id]
     return person[0] if len(person) > 0 else {}
 
@@ -40,3 +40,48 @@ def search_person(age: Optional[int] = Query(None, title="Age", description="Age
         else:
             combined = [p for p in persons1 if p in persons2]
             return combined
+    
+@app.post('/addPerson', status_code=201)        
+def add_person(person: Person):
+    p_id = max([p['id'] for p in people]) + 1 
+    new_person = {
+        "id": p_id,
+        "name": person.name,
+        "age": person.age,
+        "gender": person.gender
+    }
+    
+    people.append(new_person)
+    
+    with open('people.json', 'w') as f:
+        json.dump(people, f)
+        
+    return new_person
+
+@app.put('/changePerson', status_code=204)
+def change_person(person: Person):
+    new_person = {
+        "id": person.id,
+        "name": person.name,
+        "age": person.age,
+        "gender": person.gender
+    }
+    person_list = [p for p in people if p['id'] == person.id]
+    if len(person_list) > 0:
+        people.remove(person_list[0])
+        people.append(new_person)
+        with open('people.json', 'w') as f:
+            json.dump(people, f)
+        return new_person
+    else:
+        return HTTPException(status_code=404, detail=f"Person with id {person.id} not found")
+    
+@app.delete('/deletePerson/{p_id}')    
+def delete_person(p_id: int):
+    person = [p for p in people if p['id'] == p_id]
+    if len(person) > 0:
+        people.remove(person[0])
+        with open('people.json', 'w') as f:
+            json.dump(people, f)
+    else:
+        raise HTTPException(status_code=404, detail=f"Person with id {p_id} not found")
